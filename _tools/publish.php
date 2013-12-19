cb18e044c5482d59a03c6fe34a1b7cca02506de7
potskymini 4 Potsky <potsky@me.com>
2013-12-19 12:37:32 +0100

    pages content

--- _tools/publish.php
+++ _tools/publish.php
@@ -1,2 +1,4 @@
 <?php
-passthru(' cd ..; grunt save build install');
+# sudoer
+# %_www   ALL=(potsky) NOPASSWD: /usr/local/bin/grunt
+passthru('cd ..; PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin; export PATH; . ../../../.profile; sudo -u potsky /usr/local/bin/grunt save build install');
