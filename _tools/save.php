def133a52af37ad159877bb9c82f9af2397cce2b
potskymini 4 Potsky <potsky@me.com>
2013-12-19 12:34:44 +0100

    pages content

--- _tools/save.php
+++ _tools/save.php
@@ -1,2 +1,2 @@
 <?php
-passthru('cd ..; PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin; export PATH; . ../../../.profile; grunt save');
+passthru('cd ..; PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin; export PATH; . ../../../.profile; sudo -u potsky /usr/local/bin/grunt save build');
