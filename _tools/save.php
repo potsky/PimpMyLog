fa2c75a444d768bb8ca8cc00541f0e60e15401a7
World Wide Web Server <_www@potskymini.local>
2013-12-19 12:18:53 +0100

    commit from my ipad, pages content

--- _tools/save.php
+++ _tools/save.php
@@ -1,2 +1,2 @@
 <?php
-passthru(' cd ..; grunt save build');
+passthru('cd ..; PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin; export PATH; . ../../../.profile; grunt save build');
