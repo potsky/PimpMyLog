c3641bf1e5598c29ab1cbd9e49a392c343da9060
Potsky <potsky@me.com>
2013-12-29 19:55:46 +0100

    prepare to publish on master or beta

--- cfg/apache.paths.php
+++ cfg/apache.paths.php
@@ -6,15 +6,15 @@
  * @var  array
  */
 $paths = array(
-//	'/var/log/',
+	'/var/log/',
 	'/var/log/apache/',
-//	'/var/log/apache2/',
+	'/var/log/apache2/',
 	'/var/log/httpd/',
 	'/usr/local/var/log/apache/',
 	'/usr/local/var/log/apache2/',
 	'/usr/local/var/log/httpd/',
 	'/opt/local/apache/logs/',
-//	'/opt/local/apache2/logs/',
+	'/opt/local/apache2/logs/',
 	'/opt/local/httpd/logs/',
 	'C:/wamp/logs/',
 );
