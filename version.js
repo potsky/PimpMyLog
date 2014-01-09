b11d60337506ec7d21d0c0931f7c0aba4436aa6a
Potsky <potsky@me.com>
2014-01-09 20:33:04 +0100

    version 0.9.4

--- version.js
+++ version.js
@@ -3,6 +3,17 @@
 	"version"     : "@@VERSIONDEV",
 	"versiondevh" : "@@VERSIONDEVH",
 	"changelog" : {
+		"0.9.4" : {
+			"released" : "2014-01-09",
+			"fixed" : [
+				"Change jsonp to js extension for IE9",
+				"Fixed bootstrap 3.0.3 striped lines bug",
+				"Add default color for undefined badges type",
+				"Use strict mode in javascript",
+				"Fix a bug when slash is in a date type format",
+				"HTTP command should not be wrapped"
+			]
+		},
 		"0.9.3" : {
 			"released" : "2014-01-08",
 			"fixed" : [
