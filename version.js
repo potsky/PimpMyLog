3e01c3256951e084ecedfb16a130288885565637
Potsky <potsky@me.com>
2014-01-22 13:30:59 +0100

    prepare to publish on master or beta

--- version.js
+++ version.js
@@ -12,7 +12,7 @@
 			"new" : [
 				"Add new global parameter TITLE_FILE to customize the page title according to the current displayed file #50",
 				"Add a copy to clipboard action when configuring Pimp My Log",
-				"PML can be launched with any log file by default, not the first defined only"
+				"PML can be launched with any log file by default, not the first defined only (use http://pml/?i=apache2 for example)"
 			]
 		},
 		"0.9.8" : {
